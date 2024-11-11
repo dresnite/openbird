4c8def6b95c5698dc1e4cca57b251eef3b702413
Dresnite <giantquartz@gmail.com>
2024-11-12 00:44:16 +0100

    Implement debug levels (mostly to configure verbosity)

--- backend/src/controllers/connect-database.ts
+++ backend/src/controllers/connect-database.ts
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose'
-import { MONGODB_URI } from '../utils/config'
+import { DEBUG_LEVEL, MONGODB_URI } from '../utils/config.ts'
 
 try {
     await mongoose.connect(MONGODB_URI)
+
+    if (DEBUG_LEVEL !== -1) {
+        console.log('Successfully connected to the database')
+    }
 } catch (err) {
     console.log('Failed to connect to the database')
     process.exit()
--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -5,3 +5,5 @@ export const MONGODB_URI: string = process.env.MONGODB_URI || ''
 if (!MONGODB_URI) {
     throw new Error('The MONGODB_URI environment variable is not set')
 }
+
+export const DEBUG_LEVEL: number = process.env.DEBUG_LEVEL ? parseInt(process.env.DEBUG_LEVEL) : 0
