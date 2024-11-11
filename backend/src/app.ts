8a29bb0b6a413047ee854d73c9541b9ff03f2471
Dresnite <giantquartz@gmail.com>
2024-11-12 00:17:21 +0100

    Add user router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import userRouter from './routes/user.ts'
 import './controllers/connect-database.ts'
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(userRouter)
 
 export default app
